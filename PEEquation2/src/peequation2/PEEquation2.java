9ea61212e22f21a811e0ee78a1bfe2f078cdbdbb
ruzac <ruzac@FHC-IC0034070.wrdsb.ca>
2015-04-17 14:21:41 -0400

    This is the start with declaration.

---
+++ PEEquation2/src/peequation2/PEEquation2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package peequation2;
+
+/**
+ *
+ * @author ruzac
+ */
+public class PEEquation2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
